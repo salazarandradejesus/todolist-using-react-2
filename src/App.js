9bf90087f4fdac1a823919328f9d011a6b9e9be9
Jesús Salazar Andrade <salazarandradejesus@gmail.com>
2021-04-28 11:29:36 -0400

    por favor leer la descripcion del repositorio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [todos, setTodos] = React.useState([])
+  const [todo, setTodo] = React.useState("")
+
+
+
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  const newTodo = {
+    id: new Date().getTime(),
+    text: todo,
+    completed: false,
+  }
+
+  setTodos([...todos].concat(newTodo))
+  setTodo("")
+}
+
+function deleteTodo(id){
+  const updatedTodos = [...todos].filter((todo) => todo.id !== id)
+
+  setTodos(updatedTodos)
+}
+
+function toggleComplete(id){
+  const updatedTodos = [...todos].map((todo) => {
+    if (todo.id === id) {
+    todo.completed = !todo.completed
+    }
+    return todo
+  })
+
+setTodos(updatedTodos)
+
+}
+
+return (
+  <div className="App">
+    <form onSubmit={handleSubmit}>
+    <div className="todo-header"><h1>TodoList</h1></div>
+      <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo}></input>
+      <button type="submit">Add Todo</button>
+    </form>
+    {todos.map((todo) => <div key={todo.id}>
+      <div>{todo.text}</div>
+      <button  onClick={() => deleteTodo(todo.id)}>Delete</button> 
+      </div>)}
+  </div>
+);
 }
 
 export default App;
